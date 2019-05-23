75599dcd6c529abcd517425cb36ad976909a67e7
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-05-23 18:03:36 -0500

    Added Product model.

---
+++ models/product/product.routes.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+
+const Product = require('./product.model')
+
+router.get( '/', ( req, res ) => {
+  Product.getProducts( res, Product.responseToClient )
+})
+
+router.post( '/', ( req, res ) => {
+  let data = {
+    title: req.body.title,
+    media: req.body.media,
+    size: req.body.size,
+    image: req.body.image
+  }
+  Product.saveProduct( data, res, Product.responseToClient )
+})
+
+module.exports = router;
