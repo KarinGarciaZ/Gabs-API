e8a3c0f0a11673e6b60b0114067f41a0e33734f2
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-05-24 00:16:47 -0500

    deleted js files.

--- dist/models/admin.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require('express');
-const router = express.Router();
-const Product = require('./product/product.routes');
-const User = require('./user/user.routes');
-router.use('/product', Product);
-router.use('/user', User);
-module.exports = router;
